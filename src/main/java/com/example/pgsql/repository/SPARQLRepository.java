196b6a457332e197a61c46dd622357fd77223066
Jerome Dh <jdieuhou@yahoo.fr>
2019-05-25 11:39:37 +0100

    Update the return type for fullSearch and autocomplete methods

--- src/main/java/com/example/pgsql/repository/SPARQLRepository.java
+++ src/main/java/com/example/pgsql/repository/SPARQLRepository.java
@@ -33,15 +33,11 @@ public class SPARQLRepository
 
 		// Réquêtes vers O4TSS
 		O4TSSRDF basis1 = new O4TSSRDF();
-		list = basis1.autoCompleteQuery(list, term);
+		list.addAll(basis1.autoCompleteQuery(term));
 
 		// Réquêtes vers O4UP
 		O4UPRDF basis2 = new O4UPRDF();
-		list = basis2.autoCompleteQuery(list, term);
-
-		//
-		// ==
-		//
+		list.addAll(basis2.autoCompleteQuery(term));
 
 		return list;
 
@@ -52,92 +48,20 @@ public class SPARQLRepository
 	 */
 	public JSONResponse fullSearch(String term)
 	{
-		//Capturer le temps au début
-		long t0, t1;
-            t0 = System.currentTimeMillis();
-
-		System.out.println("\n+-------------------------------------------+");
-		System.out.println("+----------- Début d'exécution -------------+");
-		System.out.println("+-------------------------------------------+\n");
-		System.out.println("+------- Query Onto -------------+\n");
-
+		
 		// Concevoir la réponse
 		JSONResponse jsonResponse = new JSONResponse();
 
 		// Fouiller dans 04TSS
 		O4TSSRDF basis1 = new O4TSSRDF();
-		jsonResponse = basis1.fullQuery(jsonResponse, term);
+		jsonResponse.addDiseases(basis1.fullQuery(term));
 
 		// Fouiller dans O4UP
 		O4UPRDF basis2 = new O4UPRDF();
-		jsonResponse = basis2.fullQuery(jsonResponse, term);
-
-		//
-		// ==
-		//
-
-		//Calcul du temps d'exécution
-		t1 = System.currentTimeMillis();
-		System.out.println("\n+-------------------------------------------+\n");
-        System.out.println("\tTemps d'exécution: "+ (t1 - t0) + " ms");
-		System.out.println("+\n-------------------------------------------+\n");
-		
-		return jsonResponse;
-
-	}
-
-	/**
-	 * Test de réquêtes SPARQL
-	 */
-    public JSONResponse findByQuestion(String question)
-	{
-		//Résultats
-		JSONResponse jsonResponse = new JSONResponse();
-
-		//Le model
-		Model model = FileManager.get().loadModel("data/sparql-example.ttl");
-
-        String queryString = "SELECT ?p ?name ?prenom " +
-                "WHERE { " +
-                "?p <http://xmlns.com/foaf/0.1/name> ?name ." +
-                "?p <http://xmlns.com/foaf/0.1/firstname> ?prenom ." +
-                "FILTER REGEX(?prenom, \"(" +  question + ")\") " +
-                "}";
-
-        Query query = QueryFactory.create(queryString);
-     
-        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
-
-			Person person;
-            ResultSet results = qexec.execSelect();
-
-            for(; results.hasNext() ; ) {
-
-                QuerySolution soln = results.nextSolution();
-				RDFNode y = soln.get("p") ;
-				RDFNode x = soln.get("name") ;
-				RDFNode z = soln.get("prenom") ;
-
-                System.out.println("    " + y.toString()) ;
-                System.out.println("    " + x.toString()) ;
-                System.out.println("    " + z.toString()) ;
-
-				//Ajouter à la réponse
-				jsonResponse.addPerson(new Person(x.toString(), z.toString(), ""));
-
-                // Check the type of the result value
-                if ( x instanceof Literal )
-                {
-                    Literal titleStr = (Literal)x  ;
-                    System.out.println("    "+titleStr) ;
-                }
-                else
-                    System.out.println("Strange - not a literal: "+x) ;
-
-            }
-        }
+		jsonResponse.addDiseases(basis2.fullQuery(term));
 
 		return jsonResponse;
+
 	}
 
 }
