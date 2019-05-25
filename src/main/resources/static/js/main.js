7a3c7e0d28977ce59b3e975522f8757ed5b4542f
Jerome Dh <jdieuhou@yahoo.fr>
2019-05-25 11:28:26 +0100

    Update main script

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,50 +1,72 @@
 
+	/////////////////////////////////////////////////////
+	//
+	//		Script principal
+	//
+	////////////////////////////////////////////////////
 
-//alert("Bienvenue");
+
+	$(document).ready(function(){
+		//Initialisation
+		parametrage_initiale();
+
+	})
 
 
+	$( function() {
+		
+		/**
+		 * Switch en simple ou plusieurs processeurs
+		 */
+		$('#checkbox_processeur').click(getParamThread);  
+
+	});
+
+
+	/////////////////////////////////////////////////////
+	//
+	//		Utilitaires
+	//
+	////////////////////////////////////////////////////
+
+
+	/**
+	 * Initialisation des élements de la page
+	 */
+	function parametrage_initiale()
+	{
+		console.log(' Paramétrage initiale ');
+
+		//Vérifier la valeur de paramétrage des threads
+		const thread_storage = localStorage.getItem('thread');
+
+		if(thread_storage != null)
+		{
+			console.log(' thread_storage stocké ' + thread_storage);
+			var t = (thread_storage == '1') ? true : false;
+			$('#checkbox_processeur').prop('checked', t);
+		}
+	}
   
-$(document).ready(function(){
+	/**
+	 * Trouver la valeur de paramétrage des threads
+	 */
+	function getParamThread()
+	{
+		var ret = $('#checkbox_processeur').is(':checked') ? '1' : '0';
 
-      
-})
+		//Garder la préférence utilisateur
+		localStorage.setItem('thread', ret);
 
+		return ret;
 
-$( function() {
+	}
 	
-	//Afficher la mini-description
-	/* 
-	$("#result a").click(function(){
-          $("#modalInfo1").modal({
-				keyboard: false,
-				show : true,
-				backdrop: "static"
-           });
-      }); */
-
-    var availableTags = [
-      "Kolera",
-      "paludisme",
-      "Cancer",
-      "Paracetamol",
-      "Nivaquine",
-      "Cocaine",
-      "Neva",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Nsuto",
-      "Bitomo das bass",
-      "Lisp",
-      "Perl",
-      "Lorem ipsum",
-      "dolor Amed",
-      "Nimogo",
-      "Scala",
-      "Scheme"
-    ];
-
-  } ); 
+	/**
+	 * Trouver la valeur de la langue utilisateur
+	 */
+	function getUserLanguage()
+	{
+		var lang = $('#choiceLang').val();
+		return lang;
+	}
